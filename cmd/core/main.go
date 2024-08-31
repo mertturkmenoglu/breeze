ee48cb70e37edac41151f5ead6cb5ee2a15aa86c
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-31 09:40:19 +0300

    initialize main application with config setup, database migrations, and Echo framework for HTTP handling

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"breeze/config"
+	"breeze/internal/db"
+	"fmt"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/spf13/viper"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	config.Bootstrap()
+
+	shouldRunMigrations := os.Getenv("RUN_MIGRATIONS")
+
+	if shouldRunMigrations == "1" {
+		db.RunMigrations()
+	}
+
+	e := echo.New()
+
+	d := db.NewDb()
+
+	e.GET("/", func(c echo.Context) error {
+		s, err := d.Queries.Dummy(c.Request().Context())
+
+		if err != nil {
+			return err
+		}
+
+		return c.String(200, s)
+	})
+
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", viper.GetInt(config.PORT))))
 }
