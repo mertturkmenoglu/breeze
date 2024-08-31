6d232729c2869a1c690e1df6991da5b1b527d8f5
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-31 09:39:20 +0300

    add configuration package with environment loading, constants, and a development YAML file for app settings

---
+++ config/consts.go
@@ -0,0 +1,16 @@
+package config
+
+const (
+	PORT        = "port"
+	ENV         = "env"
+	DB_HOST     = "db.host"
+	DB_NAME     = "db.name"
+	DB_USER     = "db.user"
+	DB_PASSWORD = "db.password"
+	DB_PORT     = "db.port"
+	DB_TIMEZONE = "db.timezone"
+	REDIS_URL   = "redis.url"
+	REDIS_ADDR  = "redis.addr"
+	WEB_URL     = "web.url"
+	API_URL     = "api.url"
+)
