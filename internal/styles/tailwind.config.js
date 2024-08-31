88c050737543e47415312e07b9cdfce0fefb39cf
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-31 15:36:47 +0300

    refactor layout structure; add base layout and footer/navbar partials, update CSS, and enhance hello view template

--- internal/styles/tailwind.config.js
+++ internal/styles/tailwind.config.js
@@ -3,7 +3,11 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["../views/**/*.{templ,go}"],
+  content: [
+    "../views/**/*.{templ,go}",
+    "../partials/**/*.{templ,go}",
+    "../layouts/**/*.{templ,go}",
+  ],
   theme: {
     extend: {},
   },
