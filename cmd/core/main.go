6f59f587d7a3642167342e955ae3a8546dfe3d83
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-08-31 09:55:38 +0300

    render example templ component

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -3,12 +3,39 @@ package main
 import (
 	"breeze/config"
 	"breeze/internal/db"
+	"breeze/internal/views"
 	"fmt"
+	"net/http"
 	"os"
 
+	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 	"github.com/spf13/viper"
 )
+
+// This custom Render replaces Echo's echo.Context.Render() with templ's templ.Component.Render().
+func Render(ctx echo.Context, statusCode int, t templ.Component) error {
+	buf := templ.GetBuffer()
+	defer templ.ReleaseBuffer(buf)
+
+	if err := t.Render(ctx.Request().Context(), buf); err != nil {
+		return err
+	}
+
+	return ctx.HTML(statusCode, buf.String())
+}
+
+func HelloHandler(c echo.Context) error {
+	d := db.NewDb()
+
+	s, err := d.Queries.Dummy(c.Request().Context())
+
+	if err != nil {
+		return err
+	}
+
+	return Render(c, http.StatusOK, views.Hello(s))
+}
 
 func main() {
 	config.Bootstrap()
@@ -21,17 +48,7 @@ func main() {
 
 	e := echo.New()
 
-	d := db.NewDb()
-
-	e.GET("/", func(c echo.Context) error {
-		s, err := d.Queries.Dummy(c.Request().Context())
-
-		if err != nil {
-			return err
-		}
-
-		return c.String(200, s)
-	})
+	e.GET("/", HelloHandler)
 
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", viper.GetInt(config.PORT))))
 }
