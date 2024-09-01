3c3d9ff4d9fd52d91de810af8444da0a47e49422
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-02 00:35:29 +0300

    add validation functions for email, password, and name to improve user input checks in the application

---
+++ internal/app/validations.go
@@ -0,0 +1,31 @@
+package app
+
+import "strings"
+
+func isValidEmail(s string) bool {
+	if len(s) < 6 {
+		return false
+	}
+
+	containsAtSymbol := strings.Contains(s, "@")
+
+	if !containsAtSymbol {
+		return false
+	}
+
+	idx := strings.Index(s, "@")
+
+	if idx == 0 || idx == len(s)-1 {
+		return false
+	}
+
+	return true
+}
+
+func isValidPassword(s string) bool {
+	return len(s) >= 8
+}
+
+func isValidName(s string) bool {
+	return len(s) >= 2
+}
