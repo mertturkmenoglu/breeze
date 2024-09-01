3a569627cc07d5262cf9962da8c8c2ccbe1b266b
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2024-09-01 15:05:24 +0300

    refactor main.go to use app package for handlers and remove custom render logic

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -2,44 +2,14 @@ package main
 
 import (
 	"breeze/config"
+	"breeze/internal/app"
 	"breeze/internal/db"
-	"breeze/internal/views"
 	"fmt"
-	"net/http"
 	"os"
 
-	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 	"github.com/spf13/viper"
 )
-
-// This custom Render replaces Echo's echo.Context.Render() with templ's templ.Component.Render().
-func Render(ctx echo.Context, statusCode int, t templ.Component) error {
-	buf := templ.GetBuffer()
-	defer templ.ReleaseBuffer(buf)
-
-	if err := t.Render(ctx.Request().Context(), buf); err != nil {
-		return err
-	}
-
-	return ctx.HTML(statusCode, buf.String())
-}
-
-func HomeHandler(c echo.Context) error {
-	d := db.NewDb()
-
-	s, err := d.Queries.Dummy(c.Request().Context())
-
-	if err != nil {
-		return err
-	}
-
-	return Render(c, http.StatusOK, views.Home(s))
-}
-
-func LoginHandler(c echo.Context) error {
-	return Render(c, http.StatusOK, views.Login())
-}
 
 func main() {
 	config.Bootstrap()
@@ -54,8 +24,9 @@ func main() {
 
 	e.Static("/assets", "internal/assets")
 
-	e.GET("/", HomeHandler)
-	e.GET("/login", LoginHandler)
+	e.GET("/", app.HomeHandler)
+	e.GET("/login", app.LoginHandler)
+	e.POST("/login", app.ApiLogin)
 
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", viper.GetInt(config.PORT))))
 }
